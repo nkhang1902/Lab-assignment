75bee28c3dc8a113edeedfef7affb78413f626e2
nkhang1902 <81478917+nkhang1902@users.noreply.github.com>
2022-07-16 18:20:40 +0700

    Add files via upload

---
+++ assets/users.js
@@ -0,0 +1,20 @@
+
+const Users = [
+    {
+        id: 1,
+        username: 'user1', 
+        password: 'password'
+    },
+    {
+        id: 2,
+        username: 'user2', 
+        password: 'pass1234'
+    },
+    {
+        id: 3,
+        username: 'testuser', 
+        password: 'testpass'
+    },
+];
+
+export default Users
